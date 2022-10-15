8eb0f37235e9955323951b32946af6cd22a77a9e
Kushagra <64742426+A223D@users.noreply.github.com>
2022-10-15 05:41:16 -0400

    Working website sending to FB RTDB

--- myscript.js
+++ myscript.js
@@ -1,4 +1,8 @@
 function speak() {
     something = document.getElementById("speakButton").value;
     console.log(something)
+    const res = axios.put('https://manager-be70e-default-rtdb.firebaseio.com/.json', {
+        Data: something
+    });
+
 }
