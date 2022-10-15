da46697dac3bda5d11bc55f8fed7e6b8cccb9337
Kushagra <64742426+A223D@users.noreply.github.com>
2022-10-15 14:02:18 -0400

    Hardware finished

--- hardware.py
+++ hardware.py
@@ -4,8 +4,25 @@ import pygame
 import pyrebase
 import os
 from gtts import gTTS
+import board
+import digitalio
+from PIL import Image
+import adafruit_ssd1306
 
 pygame.mixer.init()
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# Change these
+# to the right size for your display!
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 5
+
+# Use for I2C.
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
 
 flag=0
 
@@ -18,6 +35,15 @@ soundsDict1={
 11: "./sounds/food.mp3"
 }
 
+imagesDict1={
+17: "./images/yes.png",
+27: "./images/no.png",
+22: "./images/bathroom.png",
+10: "./images/treat.png",
+9: "./images/walk.png",
+11: "./images/food.png"
+}
+
 soundsDict2={
 17: "./sounds/come.mp3",
 27: "./sounds/good.mp3",
@@ -27,6 +53,14 @@ soundsDict2={
 11: "./sounds/help.mp3"
 }
 
+imagesDict2={
+17: "./images/come.png",
+27: "./images/good.png",
+22: "./images/no.png",
+10: "./images/yes.png",
+9: "./images/wait.png",
+11: "./images/help.png"
+}
 
 config = {
 "apiKey": "KEY",
@@ -47,11 +81,19 @@ selector2 = Button(26)
 def playSound(soundIndex):
 	if selector1.is_pressed:
 		print("Selector1")
+		image = Image.open(imagesDict1[soundIndex]).convert('1')
+		# Display image
+		oled.image(image)
+		oled.show()
 		print("Loading audio file")
 		pygame.mixer.music.load(soundsDict1[soundIndex])
 		print("Loaded")
 	else:
 		print("Selector2")
+		image = Image.open(imagesDict2[soundIndex]).convert('1')
+		# Display image
+		oled.image(image)
+		oled.show()
 		print("Loading audio file")
 		pygame.mixer.music.load(soundsDict2[soundIndex])
 		print("Loaded")
@@ -72,6 +114,8 @@ def stream_handler(message):
 	print("Command", toSay)
 	tts = gTTS(toSay)
 	tts.save("./sounds/something.mp3")
+	oled.fill(0)
+	oled.show()
 	print("Loading audio file")
 	pygame.mixer.music.load("./sounds/something.mp3")
 	print("Loaded")
